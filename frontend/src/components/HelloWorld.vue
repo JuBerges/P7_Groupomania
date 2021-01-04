40b26b43271741fe3d8edb6665eb8c7c2851cbc6
WadeSpectre <mbergesjulien@gmail.com>
2021-01-04 11:42:07 +0100

    add: tailwind + vue(vite)

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="text-2xl border rounded shadow text-center">{{ msg }}</h1>
+  <h2>{{ cool }}</h2>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <button @click="count++" class="bg-gray-200 rounded text-4xl"> here </button>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0,
+      cool: "it works fine",
+    }
+  }
+}
+</script>
