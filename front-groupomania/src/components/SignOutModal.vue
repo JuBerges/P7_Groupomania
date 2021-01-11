55c99e632d0c9c02879cb06515b1ff40e05f7bda
WadeSpectre <mbergesjulien@gmail.com>
2021-01-11 10:57:08 +0100

    changement d'icone pour le modal de déco

--- front-groupomania/src/components/SignOutModal.vue
+++ front-groupomania/src/components/SignOutModal.vue
@@ -51,13 +51,13 @@
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                aria-hidden="true"
               >
+                <title>Déconnexion</title>
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   stroke-width="2"
-                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                 />
               </svg>
             </div>
