fea222c71b61db95a3b457d28fd87c2b47c8baa6
WadeSpectre <mbergesjulien@gmail.com>
2021-01-13 14:25:17 +0100

    add: commentaire dans profil.vue

--- front-groupomania/src/views/Profil.vue
+++ front-groupomania/src/views/Profil.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="profil h-screen pt-11">
     <h1 class="text-white bold text-5xl text-center">TEST PROFIL</h1>
+    <div><!-- img du profile --></div>
+    <div><!-- USERname email et role --></div>
+    <div><!-- Bio --></div>
+    <div>
+      <!-- profil créé le xx/xx/xxxx -->
+      <!-- profil mise à jour le xx/xx/xxxx -->
+    </div>
   </div>
 </template>
 
