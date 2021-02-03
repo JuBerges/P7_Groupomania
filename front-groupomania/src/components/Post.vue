470abebd7452fa2e7093daa7b567d3da41dc6781
WadeSpectre <mbergesjulien@gmail.com>
2021-02-03 14:23:31 +0100

    add: modal valider la suppression du post

--- front-groupomania/src/components/Post.vue
+++ front-groupomania/src/components/Post.vue
@@ -26,9 +26,9 @@
         <span class="text-gray-400 font-bold text-sm cursor-pointer pt-1"
           >Commentaires: {{ numberOfComments }}</span
         >
-        <div v-if="ownerOrAdmin">
+        <div v-if="ownerOrAdmin" class="flex">
           <svg
-            @click="deletePost()"
+            @click="toggleDelete = true"
             class="h-6 w-6 text-gray-400 cursor-pointer"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -42,6 +42,58 @@
               d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
             />
           </svg>
+          <!-- Modal pour la suppression de la publication -->
+          <div v-if="toggleDelete" class="fixed z-10 inset-0 overflow-y-auto">
+            <div
+              class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+            >
+              <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+              </div>
+              <span
+                class="hidden sm:inline-block sm:align-middle sm:h-screen"
+                aria-hidden="true"
+                >&#8203;</span
+              >
+              <div
+                class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+                role="dialog"
+                aria-modal="true"
+                aria-labelledby="modal-headline"
+              >
+                <div class="darkpost px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div class="sm:flex sm:items-start">
+                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <h3
+                        class="text-lg leading-6 font-medium text-white"
+                        id="modal-headline"
+                      >
+                        Souhaitez vous réellement supprimer cette publication ?
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="darkpost px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse"
+                >
+                  <button
+                    @click="deletePost()"
+                    type="button"
+                    class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  >
+                    Supprimer
+                  </button>
+                  <button
+                    @click="toggleDelete = false"
+                    type="button"
+                    class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  >
+                    Annuler
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="text-gray-400 font-bold text-sm flex">
           <div v-if="!toggleLike" @click="toggleLike = true">
@@ -87,6 +139,7 @@ export default {
     return {
       convertedDate: null,
       toggleLike: false,
+      toggleDelete: false,
       ownerOrAdmin: false,
     };
   },
